141139c81a458bc18deefdcf43144780f67ed57c
hna006 <hoangngocanhk4to62@gmail.com>
2024-11-21 02:52:59 +0700

    upload laptrinhc ss2coban4

---
+++ SS2coban4.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+
+    int canh = 7;
+    printf("Gia tri canh hinh vuong = %d \n", canh);
+
+    int chuVi = 7 * canh;
+    printf("Chu vi hinh vuong = %d \n", chuVi);
+
+    int dienTich = canh * canh;
+    printf("Dien tich hinh vuong = %d \n", dienTich);
+
+    return 0;
+}
