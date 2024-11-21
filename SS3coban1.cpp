a284287a4cf47c1419b4b4d43fc270e54c02d582
hna006 <hoangngocanhk4to62@gmail.com>
2024-11-22 03:29:35 +0700

    SS3coban1

---
+++ SS3coban1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    printf("hoang_ngoc_anh_ss3coban1\n");
+    printf("ten cua ban la gi?\n");
+    char name[100];
+    scanf("%100s",name);
+    printf("xin chao: %s\n",name);
+    printf("Hello world!\n");
+    return 0;
+    
+	}
