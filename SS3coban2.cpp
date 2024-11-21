0392c193dccb4e51ed980fcac205763edb73f965
hna006 <hoangngocanhk4to62@gmail.com>
2024-11-22 03:30:04 +0700

    SS3coban2

---
+++ SS3coban2.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    printf("hoang_ngoc_anh_ss3coban2\n");
+    printf("do Celsius va Fahrenheit\n");
+    float c,f;
+    scanf("%f",&c);
+    f=c+273;
+    printf("do Fahrenheit tuong ung la: %f\n",f);
+    return 0;
+}
