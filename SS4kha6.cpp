2481b837ec389536e870e4b381c538361089fc65
hna006 <hoangngocanhk4to62@gmail.com>
2024-11-25 01:09:06 +0700

    up

---
+++ SS4kha6.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() {
+    int oldIndex, newIndex;
+    int consumption;
+    long totalAmount = 0;
+    printf("Nhap chi so công to dien dau thang: ");
+    scanf("%d", &oldIndex);
+    printf("Nhap chi so công to dien cuoi thang: ");
+    scanf("%d", &newIndex);
+    if (newIndex < oldIndex) {
+        printf("Chi so moi không the nho hon chi so cu.\n");
+        return 1;  
+    }
+    consumption = newIndex - oldIndex;
+    printf("Luong dien tiêu thu trong thang: %d kWh\n", consumption);
+    if (consumption <= 50) {
+        totalAmount = consumption * 1500;  
+    } else if (consumption <= 100) {
+        totalAmount = 50 * 1500 + (consumption - 50) * 2000;  
+    } else {
+        totalAmount = 50 * 1500 + 50 * 2000 + (consumption - 100) * 2500;  
+    }
+    printf("So tien dien phai tra la: %ld VND\n", totalAmount);
+    return 0;
+}
